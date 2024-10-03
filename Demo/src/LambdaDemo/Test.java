587bcdbad75cd058bae19f7df204590e031de65e
yadavprerana455 <yadavprerana455@gmail.com>
2024-10-03 19:00:54 +0530

    Second coomit

--- Demo/src/LambdaDemo/Test.java
+++ Demo/src/LambdaDemo/Test.java
@@ -29,3 +29,9 @@ public class Test{
 		i.m1();
 	}
 }
+public class Test{
+	public static void main(String[] args) {
+		interf i =()->System.out.println("M1 methos ipmplemetation");
+		i.m1();
+	}
+}
